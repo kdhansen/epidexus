fe418cf272b992e370721a86ef3785fd418f118b
Karl Damkjær Hansen <kdh@es.aau.dk>
2020-04-15 23:34:02 +0200

    Add license

--- epidexus.py
+++ epidexus.py
@@ -1,3 +1,19 @@
+# Epidexus - Agent Based Location-Graph Epidemic Simulation
+# Copyright (C) 2020  Karl D. Hansen <kdh@es.aau.dk>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 from bisect import insort
 from copy import copy
 from datetime import datetime, timedelta
